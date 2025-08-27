76f775cb91a6aa05007a73d92d76e5b7ab9ec401
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-08-27 13:44:03 +0300

    Add container, row, column to DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+// Etch-a-Sketch Project
+
+const grid = document.querySelector('#container');
+const row = document.querySelector('.row');
+const column = document.querySelector('.column');
+
+
+
+
+const createGrid = (rows, columns) = {
+
+};
